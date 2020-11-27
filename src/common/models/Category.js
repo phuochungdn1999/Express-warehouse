33b8e9bc2fe30d387d5957c882feb989ffdeed05
Thang Do <18thangd@gmail.com>
2020-11-27 09:25:30 +0700

    Update throwing error when validation failed

--- src/common/models/Category.js
+++ src/common/models/Category.js
@@ -40,7 +40,7 @@ function validateCategory(req, res, next) {
     abortEarly: false
   })
   // response when having error
-  if (error) return res.status(400).json({ statusCode: 400, message: error.message })
+  if (error) throw new BadRequestError(error.message)
   else next() // no error
 }
 
