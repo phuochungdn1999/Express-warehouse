12ef1a4ab8d112a95e3aa35d9df194e1f5463fee
Thang Do <18thangd@gmail.com>
2020-12-21 16:30:43 +0700

    Update authorization

--- src/app/users/controller/index.js
+++ src/app/users/controller/index.js
@@ -2,12 +2,17 @@ const router = require('express').Router()
 const { validateUser } = require('../../../common/models/User')
 const service = require('../service')
 const { auth } = require('../../../common/middlewares/auth')
+const { checkAction } = require('../../../common/middlewares/check-action')
 
 router.get('/', async (req, res) => {
   return await service.getAll(req, res)
 })//done
 
-router.post('/', [validateUser], async (req, res) => {
+router.post('/', [
+  auth, 
+  checkAction(['CREATE_USER']),
+  validateUser
+], async (req, res) => {
   return await service.createOne(req, res)
 })//done
 
@@ -15,7 +20,11 @@ router.get('/:id', async (req, res) => {
   return await service.getOne(req, res)
 })
 
-router.patch('/:id', [auth, validateUser], async (req, res) => {
+router.patch('/:id', [
+  auth, 
+  checkAction(['EDIT_USER']),
+  validateUser
+], async (req, res) => {
   return await service.updateOne(req, res)
 })
 
@@ -27,7 +36,10 @@ router.get('/search/:text',[auth],async (req, res)=>{
   return await service.search(req,res)
 })
 
-router.delete('/:id', [auth], async (req, res) => {
+router.delete('/:id', [
+  auth,
+  checkAction(['DELETE_USER'])
+], async (req, res) => {
   return await service.deleteOne(req, res)
 })
 
